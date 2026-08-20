fcb4326730da0337c559c3373cc08cf53a38aff3
Sanskar Sarak <sanskarsarak91@gmail.com>
2026-08-20 23:04:49 +0530

    Add files via upload

---
+++ For_Loop.py
@@ -0,0 +1,151 @@
+
+#============================= For Loop ============================================
+
+#1. print numbers from 1 to 10.
+# for i in range(1,11):
+#     print(i)
+
+#2. print numbers from 10 to 1
+# for a in range(10,0,-1):    
+#     print(a)
+
+#3. print all even numbers from 1 to 20
+# for i in range(0,21,2):
+#     print(i)
+
+#4. print all odd numbers from 1 to 20
+# for i in range(1,20,2):
+#     print(i)
+
+#5. take a number from the user and print its multiplication table from 1 to 20
+# a=int(input("Enter the number:"))
+# for i in range(1,11):
+#     print(a*i)
+
+#6. find the sum of numbers from 1 to 10.
+# for i in range(1,11):
+#     a=i+i
+#     print(a)
+
+#7. find the sum of even numbers from 1 to 20.
+# for i in range(0,20,2):
+#     a=i+i
+#     print(a)
+
+#8. find the sum of odd numbers from 1 to 20.
+# for i in range(1,20,2):
+#     a=i+i
+#     print(a)
+
+#9. take a word from the user and print each character on a new line.
+# char=input("Enter Your Name:")
+# for i in char:
+#     print(i)
+
+#10. take a word from the user and count the number of character.
+# name=input("Enter Your Name:")
+# count=0
+# for ch in name:
+#     count+=1
+#     print(count)
+
+#11. take a string and count the vowels in it
+# a=input("Enter Your name:")
+# count=0
+# for i in a:    
+#     if i in "aiueo":
+#         count+=1
+#         print(count)
+        
+#12. create a list of 5 names and print each name.
+# list=["sanskar", "samadhan", "soham", "rohit", "rahul"]
+# for i in list:
+#     print(i)
+
+#13. find the largest number in a list.
+# num=[1,2,3,30,4,5,6,7,8]
+# large=num[0]
+# for i in num:   
+#     if i>large:
+#         large=i
+# print(large)
+
+#14. find the smallest number in a list.
+# numbers=[3,4,5,6,1,2,7,8,9]
+# smallest=numbers[0]
+# for a in numbers:
+#     if a<smallest:
+#         smallest=a
+# print(smallest)
+
+#15. count how many positive numbers are in a list
+# list=[1,-2,3,-4,5,90,7,-7,5,54,-2,-4,-2,2,-1,3,4,87,8,8,-96]
+# count=1
+# for a in list:
+#     if a>=0:
+#         print(count)
+#         count+=1
+
+#16. count how many negative numbers are in a list
+# list=[1,-2,3,-4,5,90,7,-7,5,54,-2,-4,-2,2,-1,3,4,87,88,-96]
+# count=-1
+# for a in list:
+#     if a<=0 and a<0:
+#         print(count)
+#         count-=1
+
+#17. print all numbers from 1 to 50 that are divided by 5.
+# for i in range(1,51):
+#     if i%5==0:
+#         print(i)
+
+#18. find the factorial of the number.
+# num=int(input("Enter Any Number:"))
+# fact=1
+# for i in range(1, num+1):
+#     fact=fact*i
+#     print(fact)
+
+#19. take a string and print it in reverse.
+# text=input("Enter Any Text:")
+# reverse=""
+# for i in text:
+#     reverse=i+reverse
+# print(reverse)
+
+#20. take a string and a character from the user. count how many times the character appears in the string.
+# text=input("Enter the Text:")
+# char=input('enter a character:')
+# count=0
+# for ch in text:
+#     if ch==char:
+#         count+=1
+# print("Count:", count)
+
+#21. print multiples of 3 from 1 to 30.
+# for i in range(1,31):
+#     if i%3==0:
+#         print(i)
+
+#22. print number from 50 to 1
+# for i in range(51,1,-1):
+    
+#     print(i)
+
+#23. take a number from user and print all numbers from 1 to that number
+# num=int(input("Enter Any number:"))
+# for i in range(1,num-0):
+#     print(i)
+
+#============================= Thank You ===================================
+
+#========================= This is Just Part-1 ===============================
+    
+
+    
+    
+    
+    
+   
+
+    
